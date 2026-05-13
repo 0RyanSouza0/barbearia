30930ceabfa0f64402efa7351cc948eec568536a
0RyanSouza0 <tlousouza678@gmail.com>
2026-05-13 12:44:16 -0300

    chore: ignore env files

--- env.barbearia.sh
+++
@@ -1,4 +0,0 @@
-export ConnectionStrings__DefaultConnection="Server=localhost;Database=Barbearia;User Id=sa;Password=Ryandias15@;TrustServerCertificate=True;"
-export Jwt__Key="cRBFGLXzvHWi&NoJ8La%RvWcHU8x6^yu"
-export Jwt__Issuer="Barbearia"
-export Jwt__Audience="Barbearia"
